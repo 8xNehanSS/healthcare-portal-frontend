4dbce35d74dd2ad5959271944de4bc8d0202d80c
8xNehanSS <nehan.sudasinghe@gmail.com>
2024-07-05 15:35:10 +0530

    react router implementation with nav layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,56 @@
+// package imports
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import { Provider } from "react-redux";
+
+// redux imports
 import { store } from "./state/store";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./state/store";
+import { endLoader } from "./state/loader/loaderSlice";
+
+// pages
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import AboutUs from "./pages/About";
+
+// components
+import Loader from "./components/common/Loader";
+import "./index.css";
+
+export default function App() {
+  const loader = useSelector((state: RootState) => state.loader.value);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      dispatch(endLoader());
+    }, 2000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
+  if (loader) {
+    return <Loader />;
+  }
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="about-us" element={<AboutUs />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
