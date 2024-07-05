f8a87c7373d48ed771fa0aa49bc8b30a7943b745
8xNehanSS <nehan.sudasinghe@gmail.com>
2024-07-05 14:51:18 +0530

    loader redux reducer created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Counter from "./components/common/Counter";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counter/counterSlice";
+import loaderReducer from "./loader/loaderSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    loader: loaderReducer,
   },
 });
 
