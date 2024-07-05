71847f7c12f25d159f19f455ba1a21dcdd20e9ea
8xNehanSS <nehan.sudasinghe@gmail.com>
2024-07-05 13:51:37 +0530

    redux basic implementation

---
+++ src/components/Counter.tsx
@@ -0,0 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../state/store";
+import { decrement, increment } from "../state/counter/counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h2>{count}</h2>
+      <div>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counter/counterSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+  },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
