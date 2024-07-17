1efee3c05e086251e3b93953f3dfd84dc6fdb974
8xNehanSS <nehan.sudasinghe@gmail.com>
2024-07-17 13:42:09 +0530

    doctor dash section 1 design

--- src/pages/dashboard/doctor/DoctorDash.tsx
+++ src/pages/dashboard/doctor/DoctorDash.tsx
@@ -1,7 +1,47 @@
+import "./DoctorDash.css";
+
 const DoctorDash = () => {
+  const today_appointments = 2;
+  const today_comp_appointments = 1;
+  const appointment_requests = 3;
   return (
-    <div>
-      <h1>Doctor Dashboard</h1>
+    <div className="docdash">
+      <h2 className="docdash-heading">Doctor Dashboard</h2>
+      <div className="docdash-section1">
+        <div className="docdash-section1-container">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="Avatar"
+            className="docdash-section1-img"
+          />
+          <div>
+            <h3>Today's Appointments</h3>
+            <p className="docdash-section1-number">{today_appointments}</p>
+          </div>
+        </div>
+        <div className="docdash-section1-container">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="Avatar"
+            className="docdash-section1-img"
+          />
+          <div>
+            <h3>Compeleted Appointments</h3>
+            <p className="docdash-section1-number">{today_comp_appointments}</p>
+          </div>
+        </div>
+        <div className="docdash-section1-container">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="Avatar"
+            className="docdash-section1-img"
+          />
+          <div>
+            <h3>Appointment Requests</h3>
+            <p className="docdash-section1-number">{appointment_requests}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
